57afe06c69cead0db2ef2f8fbac0331dc2ee3216
jyz16 <jyz116@ic.ac.uk>
2018-05-31 14:04:57 +0100

    [ys3316][jyz16] Set up server using node js, removed conflict bootstrap link in TutorList.html.

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/frontend'));
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/frontend/HTML/index.html');
+});
+
+app.get('/TutorList.html', function(req, res) {
+	res.sendFile(__dirname + '/frontend/HTML/TutorList.html');
+});
+
+app.get('/code.html', function(req, res) {
+	res.sendFile(__dirname + '/frontend/HTML/code.html');
 });
 
 http.listen(8080, function() {
