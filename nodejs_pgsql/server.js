8ed3563a5aaf5f174b1441856998d1a2916c464f
Wai Lee <wcl16@ic.ac.uk>
2018-06-01 02:18:44 +0100

    Added link to tutorList.html after log in

--- nodejs_pgsql/server.js
+++ nodejs_pgsql/server.js
@@ -2,52 +2,48 @@ const express = require('express');
 const app = express();
 const pg = require('pg');
 
+// Connect to users database
+const pool = new pg.Pool({
+    user: 'g1727138_u',
+    host: 'db.doc.ic.ac.uk',
+    database: 'g1727138_u',
+    password: 'fACG1SB1c9',
+    port: '5432'
+});
 
-app.use(express.static(__dirname));
+const port = 8000;
 
-//const connectionString = 'postgres://db.doc.ic.ac.uk:5432/g1727138_u';
-//const client = new pg.Client(connectionString);
-//client.connect();
-//const query = client.query()
-
-/*const pool = new pg.Pool({
-user: 'g1727138_u',
-host: 'db.doc.ic.ac.uk',
-database: 'g1727138_u',
-password: 'fACG1SB1c9',
-port: '5432'});
-pool.query("CREATE TABLE users(username VARCHAR(40) PRIMARY KEY, password VARCHAR(40) )", (err, res) => {
-console.log(err,res);
-pool.end();
-});*/
-
-
+app.use(express.static(__dirname ));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/login.html')
 });
 
+// When "Login" button is clicked ...
 app.get('/process_get', function(req, res){
-    response = {
-        username : req.query.username,
-        password : req.query.password
-    };
-    const pool = new pg.Pool({
-user: 'g1727138_u',
-host: 'db.doc.ic.ac.uk',
-database: 'g1727138_u',
-password: 'fACG1SB1c9',
-port: '5432'});
-pool.query("INSERT INTO users(username, password) VALUES('" + response.username + "', '" + response.password + "')", (err, res) => {
-    console.log(err,res);
-    pool.end();
-});
-    //const str = `${response.username} asdfjkj`;
-    //console.log(str);
-    //convert the response in JSON format
-    res.end(JSON.stringify(response));
+
+    // Query database for user and password
+    const query = "SELECT DISTINCT username, password FROM users WHERE username = '" + req.query.username + "'";
+
+    pool.query(query, (err, result) => {
+        // If username is not in database ...
+        if (result.rowCount == 0) {
+            // ... Reload login page
+            res.sendFile(__dirname + '/login.html');
+        // If username and password is correct ...
+        } else if (result.rows[0].password === req.query.password) {
+            // ... Load tutor page
+            res.sendFile(__dirname + '/tutorList.html');
+        // Otherwise, password is incorrect ...
+        } else {
+            // ... Reload login page
+            res.sendFile(__dirname + '/login.html');
+        }
+    });
+
 });
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 8080!')
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
