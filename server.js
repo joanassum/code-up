a59ef60bdf5608a52181744c05a232f9576728e8
Wai Lee <wcl16@ic.ac.uk>
2018-06-06 15:02:55 +0100

    Implemented working Chat function

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 const pg = require('pg');
 
 // Connect to users database
@@ -11,10 +13,18 @@ const pool = new pg.Pool({
     port: '5432'
 });
 
+
+// A dictionary of online users
+const users = {};
+
 const port = 8000;
+server.listen(port);
 
 // Specify to serve files from the public directory
 app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 // HTML directory
 const html_dir = __dirname + '/public/html/';
@@ -36,8 +46,12 @@ app.get('/login', function(req, res){
             res.sendFile(html_dir + 'login.html');
         // If username and password is correct ...
         } else if (result.rows[0].password === req.query.password) {
-            // ... Load tutor page
-            res.sendFile(html_dir + 'tutorList.html');
+            //... Load tutor page
+            res.render('pages/tutorList', {
+                username: req.query.username,
+                users: [{name: 'Amelie'}, {name: 'alvis'}]
+            });
+
         // Otherwise, password is incorrect ...
         } else {
             // ... Reload login page
@@ -51,7 +65,19 @@ app.get('/code', function(req, res) {
     res.sendFile(html_dir + 'code.html');
 });
 
+io.sockets.on('connection', function(socket) {
 
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port + '!');
+    socket.on('login', function(data) {
+        socket.username = data;
+        users[socket.username] = socket;
+    });
+
+    socket.on('to server', function(data) {
+        users[data.to].emit('to client', {from: data.from, msg: data.msg});
+    });
+
+    socket.on('disconnect', function() {
+        // Remove the user from the online list
+        delete users[socket.username];
+    });
 });
