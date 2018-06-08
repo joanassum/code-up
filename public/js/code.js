9367c28a32d8b5cbeeb8915e1d1408020c52c589
ys3316 <ys3316@ic.ac.uk>
2018-06-08 05:12:28 +0100

    merge conflict solve

--- public/js/code.js
+++ public/js/code.js
@@ -3,5 +3,8 @@ $(document).ready(function(){
     // Setup connection
 	let socket = io();
 	socket.emit('login', $('#username').text());
-    socket.emit('to server', {from: $('#username').text(), to: $('#tutorID').text(), msg: "Please Code with me: <a href='" + $(location).attr('href') + "''>" + $(location).attr('href') + "</a>"});
+
+	let link = $(location).attr('href').slice(0, $(location).attr('href').indexOf('=') + 1) + encodeURIComponent($('#tutorID').text()) + $(location).attr('href').slice($(location).attr('href').indexOf('&tutorID='));
+
+    socket.emit('to server', {from: $('#username').text(), to: $('#tutorID').text(), msg: "Please Code with me: <a href='" + link + "''>" + link + "</a>"});
 });
